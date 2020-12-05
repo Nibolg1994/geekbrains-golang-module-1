0934639727b7df30002c8f9bd5d7073d22a68e58
nibolg1994 <globin.anvi@gmail.com>
2020-12-05 17:53:51 +0300

    added validation and new operation

--- lesson-2/calc.go
+++ lesson-2/calc.go
@@ -3,19 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	var a, b, res float32
+	var a, b, res float64
+	var inputa, inputb string
 	var op string
 
 	fmt.Print("Введите первое число: ")
-	fmt.Scanln(&a)
+	fmt.Scanln(&inputa)
+	a, err := strconv.ParseFloat(inputa, 64)
+	if err != nil {
+		fmt.Println("Введены не корректные данные")
+		os.Exit(1)
+	}
 
 	fmt.Print("Введите второе число: ")
-	fmt.Scanln(&b)
+	fmt.Scanln(&inputb)
+	b, err = strconv.ParseFloat(inputb, 64)
+	if err != nil {
+		fmt.Println("Введены не корректные данные")
+		os.Exit(1)
+	}
 
-	fmt.Print("Введите арифметическую операцию (+, -, *, /): ")
+	fmt.Print("Введите арифметическую операцию (+, -, *, /, %): ")
 	fmt.Scanln(&op)
 
 	switch op {
@@ -27,10 +39,17 @@ func main() {
 		res = a * b
 	case "/":
 		res = a / b
+	case "%":
+		if a == float64(int64(a)) && b == float64(int64(b)) {
+			res = float64(int64(a) % int64(b))
+		} else {
+			fmt.Println("В этой операции должны быть целые числа")
+			os.Exit(1)
+		}
 	default:
 		fmt.Println("Операция выбрана неверно")
 		os.Exit(1)
 	}
 
-	fmt.Printf("Результат выполнения операции: %d\n", res)
+	fmt.Printf("Результат выполнения операции: %f\n", res)
 }
